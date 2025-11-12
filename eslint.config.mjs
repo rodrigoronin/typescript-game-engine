ff81d2249cccf5b7dc116aab6bc251e1c1e47687
rodrigoronin <liramailbox@gmail.com>
2025-11-12 17:50:21 -0300

    fixing eslint prettier conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,6 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      prettier,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
